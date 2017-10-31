0c6c20ea42e23873615e8589381dd983625e4df0
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-10-31 11:09:00 +0900

    Migrate to use v0.14 API

--- test/plugin/test_parser_base64.rb
+++ test/plugin/test_parser_base64.rb
@@ -8,32 +8,24 @@ include Fluent
 
 class Base64ParserTest < ::Test::Unit::TestCase
   def test_encode
-    parser = TextParser::Base64Parser.new
+    parser = Plugin::Base64Parser.new
     parser.configure('message_key' => 'msg', 'base64_encode' => true)
 
-    # for the old, return based API
     text = 'foo'
     expected_record = {'msg' => 'Zm9v' }
-    time, record = parser.parse(text)
-    assert_equal(expected_record, record)
 
-    # for the new API
     parser.parse(text) {|time, record|
       assert_equal(expected_record, record)
     }
   end
 
   def test_decode
-    parser = TextParser::Base64Parser.new
+    parser = Plugin::Base64Parser.new
     parser.configure('message_key' => 'msg', 'base64_encode' => false)
 
-    # for the old, return based API
     text = 'Zm9v'
     expected_record = {'msg' => 'foo' }
-    time, record = parser.parse(text)
-    assert_equal(expected_record, record)
 
-    # for the new API
     parser.parse(text) {|time, record|
       assert_equal(expected_record, record)
     }
